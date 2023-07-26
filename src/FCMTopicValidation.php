edbd50c6c0b004347280878a74ee2305686df5e0
Mohamed Abdulalim <megyptm@gmail.com>
2023-07-27 02:52:06 +0300

    1.1.1

--- src/FCMTopicValidation.php
+++ src/FCMTopicValidation.php
@@ -6,6 +6,25 @@
  * Time: 12:07 AM
  * https://www.Maatify.dev
  */
+/**
+ * @copyright   ©2023 Maatify.dev
+ * @Liberary    Logger
+ * @Project     Logger
+ * @author      Mohamed Abdulalim (megyptm) <mohamed@maatify.dev>
+ * @since       2023-07-27 12:07 AM
+ * @see         https://www.maatify.dev Maatify.com
+ * @link        https://github.com/Maatify/FCM  view project on GitHub
+ * @link        https://github.com/Maatify/Logger/ (maatify/logger),
+ * @link        https://github.com/kreait/firebase-php/ (kreait/firebase-php),
+ * @copyright   ©2023 Maatify.dev
+ * @note        This Project using for Google Firebase Cloud Message.
+ * @note        This Project extends other libraries kreait/firebase-php, maatify/logger
+ *
+ * @note        This program is distributed in the hope that it will be useful - WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ */
 
 namespace Maatify\FCM;
 
